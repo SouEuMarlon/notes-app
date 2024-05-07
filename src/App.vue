5b607a24f3ff11dc02154bdcda926ae7eda07f65
Marlon <marlon_menezes@outlook.com>
2024-05-07 18:46:43 -0300

    Feat: Local Storage and delete note function included on project. Projet ended.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 const showModal = ref(false);
 const newNote = ref("");
 const errorMessage = ref("");
 const notes = ref([]);
+
+onMounted(() => {
+  // Carregar notas do localStorage quando o componente é montado
+  if (localStorage.getItem('notes')) {
+    notes.value = JSON.parse(localStorage.getItem('notes'));
+  }
+});
+
+const saveNotesToLocalStorage = () => {
+  localStorage.setItem('notes', JSON.stringify(notes.value));
+}
 
 const openAndCloseModal = () => {
   showModal.value = !showModal.value;
@@ -16,8 +27,7 @@ function getRandomColor(){
 }
 
 const addNote = () => {
-  if(newNote.value === ""){
-    
+  if(newNote.value === "" || newNote.value.trim() === ""){ 
     errorMessage.value = "Please enter a note!";
     return;
   } else {
@@ -30,7 +40,13 @@ const addNote = () => {
     });
     newNote.value = "";
     showModal.value = false;
+    saveNotesToLocalStorage();
   }
+}
+
+const removeNote = (noteId) => {
+  notes.value = notes.value.filter(note => note.id !== noteId);
+  saveNotesToLocalStorage(); // Salvar as notas no localStorage após remover
 }
 
 </script>
@@ -39,7 +55,7 @@ const addNote = () => {
   <main>
     <div class="overlay" v-if="showModal">
       <div class="modal">
-        <textarea name="note" id="note" cols="30" rows="10" v-model="newNote"></textarea>
+        <textarea name="note" id="note" cols="30" rows="10" v-model.trim="newNote"></textarea>
         <p class="error-message" v-if="errorMessage">{{errorMessage}}</p>
         <button class="modal-button" @click="addNote">Add Note</button>
         <button class="modal-close-button" @click="openAndCloseModal">Close</button>
@@ -55,7 +71,12 @@ const addNote = () => {
           <p class="main-text">
             {{ note.text }}
           </p>
-          <p class="date">{{note.date}}</p>
+          <div class="date-and-delete">
+            <p class="date">{{ note.date }}</p>
+            <button class="delete-button" @click="removeNote(note.id)">
+              <svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="20px" height="20px" viewBox="0 0 485 485" xml:space="preserve" transform="rotate(0)" stroke="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <rect x="67.224" width="350.535" height="71.81"></rect> <path d="M417.776,92.829H67.237V485h350.537V92.829H417.776z M165.402,431.447h-28.362V146.383h28.362V431.447z M256.689,431.447 h-28.363V146.383h28.363V431.447z M347.97,431.447h-28.361V146.383h28.361V431.447z"></path> </g> </g> </g></svg>
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -133,6 +154,38 @@ const addNote = () => {
     scrollbar-color: #000 transparent;
   }
 
+  .date-and-delete{
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .date-and-delete button{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: transparent;
+    border-radius: 3px;
+    border: none;
+    color: #fff;
+    cursor: pointer;
+    padding: 3px;
+    transition: background-color 0.3s ease-in-out;
+  }
+
+  .delete-button svg{
+    fill: #000;
+    transition: fill 0.3s ease-in-out;
+  }
+
+  .delete-button svg:hover{
+    fill: #fff;    
+  }
+
+  .delete-button:hover{
+    background-color: rgb(191, 33, 33, 0.9);
+  }
+
   .date{
     color: #212121;
     font-size: 0.75rem;
@@ -158,6 +211,7 @@ const addNote = () => {
     padding: 20px;
     max-width: 700px;
     width: 100%;
+    z-index: 1;
   }
 
   .error-message{
@@ -218,6 +272,13 @@ const addNote = () => {
     }
   }
 
+  @media (max-width: 979px){
+    .cards-container{
+      padding: 0 10px;
+      justify-content: center;
+    }
+  }
+
   @media (max-width: 510px){
     
     h1{
@@ -228,9 +289,10 @@ const addNote = () => {
       height: 46px;
       width: 46px;
     }
+  }
 
+  @media (max-width: 500px){
     .cards-container{
-      justify-content: center;
       padding: 0;
     }
   }
